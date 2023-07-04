8000d8b99cff7287974ad534037a75de0f60f53d
eaintm <EAINTPHOOMAY@GMAIL.COM>
2023-07-04 00:05:11 -0400

    add instagram logo to the left side of the header component

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function Header() {
+    return <div>
+        {/* Left */}
+        <div className="flex items-center justify-between max-w-6xl">
+            <div className ="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+                <Image 
+                src ="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png" 
+                layout='fill'
+                className = "object-contain"
+                />
+            </div>    
+            <div className ="cursor-pointer h-24 w-10 relative lg:hidden"> 
+                <Image
+                src = "https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg" 
+                layout='fill'
+                className='object-contain'
+            />
+            </div>
+            <h1>Right Side</h1>
+        </div>
+        {/*Middle*/}
+
+        {/*Right */} 
+     
+  </div>
+}
---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from '../../components/Header'
+ 
+export default function Home() {
+  return (
+      <div>
+        <Head>
+          <title>Instagram</title> 
+        </Head>
+        {/*Header*/}
+        <Header/>
+       
+
+        {/*Feed*/}     
+
+        {/* Modal */}
+      </div>
+
+  )
+}
